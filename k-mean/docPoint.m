b3c2e708e05841230d39aa06eb9889fe30cb1a55
Yunlu Wen <wylswz@icloud.com>
2016-11-12 14:52:27 +0000

    k-nn

--- k-mean/docPoint.m
+++ k-mean/docPoint.m
@@ -2,11 +2,17 @@ classdef docPoint
    properties
        coord;
        cluster;
+       id;
    end
    methods
-       function docPoint = docPoint(coord,cluster)
+       function docPoint = docPoint(id,coord,cluster)
+                docPoint.id = id;
                 docPoint.coord = coord;
                 docPoint.cluster = cluster;
+       end
+       
+       function idOut = getID(coord)
+                
        end
    end
     

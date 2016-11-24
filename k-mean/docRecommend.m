c5245c75bf9b202fcc1bd02f725f274e355e833e
Johnny <wylswz@icloud.com>
2016-11-24 13:36:41 +0000

    asd

---
+++ k-mean/docRecommend.m
@@ -0,0 +1,9 @@
+function recoList = docRecommend(newDoc,k,C,IDX,dimen)
+recNum = [4 3 2 1]; %% return 10 articles
+nearK = knn(C(:,1:dimen),newDoc(1:dimen),'K',4);
+for i=1:1:4
+    index = nearK(i);
+    txtidx = textInTopic(index,recNum(i),IDX);
+    recoList = [recoList txtidx];
+end
+end
--- k-mean/textInTopic.m
+++ k-mean/textInTopic.m
@@ -1,9 +1,11 @@
-function textInTopic(k,num,IDX)
+%return some documents in a certain cluster
+function [txtidx] = textInTopic(k,num,IDX)
 numLimit = histc(IDX,k);
 if num >= numLimit
    num = numLimit;
 end  %%if that topic do not have so many aricles, just retuen all of the topics it has
- 
-
+base = find(ismember(IDX,k)); %the base to choose from
+index = randperm(numLimit,num);
+txtidx = base(index)
 
 end

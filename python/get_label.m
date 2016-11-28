7706bccd4ac3f28c570dc164b03e178c6380e482
johnny <wylswz@icloud.com>
2016-11-28 15:52:52 +0000

    Implemented the segment filter to shrink the doc-term matrix Add a script to plot the simple scatter

--- python/get_label.m
+++ python/get_label.m
@@ -1,8 +1,11 @@
-function label = get_label(tokens,U,no,dimen)
-label = strings(dimen,1)
+function label = get_label(tokens,U,no,dimen) 
+%%no indicates which dimension
+%dimens indicates how many keywords to display
+labels = strings(1,dimen);
 test = U(:,no);
 [weight index] = sort(test,'descend');
 for i = 1:1:dimen
-     label(i) = strcat(num2str(weight(i)),tokens(index(i)));
+     labels(i) = strcat(num2str(weight(i)),tokens(index(i)));
 end
+label = strcat(labels);
 end
--- python/script.m
+++ python/script.m
@@ -10,6 +10,17 @@ hold on
 scatter3(V([13:15 28:30 43:45],2),V([13:15 28:30 43:45],3),V([13:15 28:30 43:45],4))
 
 
+legend('Computer','Battle','Music','Religion','Electronic')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+text(V_NEW(1,2),V_NEW(1,3),V_NEW(1,4),'history');
+text(V_NEW(2,2),V_NEW(2,3),V_NEW(2,4),'electronic');
+text(V_NEW(3,2),V_NEW(3,3),V_NEW(3,4),'music');
+text(V_NEW(4,2),V_NEW(4,3),V_NEW(4,4),'religion');
+text(V_NEW(5,2),V_NEW(5,3),V_NEW(5,4),'computer');
+
+
 %{
 newTextPlot(V,44,1,2,3,4)
 newTextPlot(V,58,1,2,3,4)

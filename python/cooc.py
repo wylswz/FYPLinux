83b9d00cdb5b3d7e7244bc9d4d24e7045220ce65
johnny <wylswz@icloud.com>
2016-12-06 16:20:19 +0000

    seems like nothing changes

--- python/cooc.py
+++ python/cooc.py
@@ -20,15 +20,15 @@ def get_tokens(dir):
          return tokens
 
 def get_idf_co(c,w,tokens):
-    N = 10
+    N = len(tokens)
     NC = 0.001
     NW = 0.001
     co = 0   
-    for iter in range(1,N+1):
+    for iter in range(0,N):
         tf_c = 0
         tf_w = 0
-        tf_c = tokens[iter-1].count(c)
-        tf_w = tokens[iter-1].count(w)
+        tf_c = tokens[iter].count(c)
+        tf_w = tokens[iter].count(w)
         if tf_c > 0:
            NC = NC+1
         if tf_w >0:
@@ -45,24 +45,26 @@ def coov_builder(concept,query):
     delta = 0.1
     coov = np.zeros((R,C))
     conceptList = get_tokens('conRange/list')
-    tokens = [' ' for i in range(0,len(conceptList))]
-    for iter in range(0,len(conceptList)):
+   # tokens = [' ' for i in range(0,len(conceptList))]
+    tokens = [' ' for i in range(0,4000)]
+   # for iter in range(0,len(conceptList)):
+    for iter in range(0,4000):
         ####################################add a concept list here
-        tokens[iter] = get_tokens('wikiStemed/' + conceptList[iter]) 
-    for i in range(0,R):##R is num of concept
+        tokens[iter] = get_tokens('wikiStemed/' + str(iter+1)) 
+    for i in range(0,R):  ##R is num of concept
         for j in range(0,C): #num of query terms  
             (idf,idfW,co) = get_idf_co(concept[i],query[j],tokens) 
             #print idf,co         
             coov[i,j] = math.pow((math.log(co+1)/math.log(10)*idf/math.log(10) + delta),idfW)
             #print coov[i,j]
         print i
-    return coov
+    return coov 
 
 
 concept = get_tokens('keys')
 query = get_tokens('testStemed/8')
 
-coov = coov_builder(concept,query)
+coov = coov_builder(concept,['car','manufact'])
 coov1 =np.sort(np.prod(coov,axis = 1))
 index = np.argsort(np.sum(coov,axis = 1))
 token = get_tokens('keys')
